8d135792042471a027f5bd74bb94c5e88c86d1a1
坂井 makoto <>
2022-08-23 12:45:53 +0900

    create => group & Map Pages

--- assets/js/common.js
+++ assets/js/common.js
@@ -103,10 +103,27 @@ $(window).on("load resize", function () {
 	}
 });
 
-
 $(function () {
 	$('.popup__button--open').on("click", function () {
 	  $(this).next('div').slideToggle();
 	  $(this).find(".icon").toggleClass('open');
 	});
 });
+
+// 汎用 スムーズスクロール
+const smoothScrollTrigger = document.querySelectorAll('a[href^="#"]');
+for (let i = 0; i < smoothScrollTrigger.length; i++) {
+	smoothScrollTrigger[i].addEventListener('click', (e) => {
+		e.preventDefault();
+		let href = smoothScrollTrigger[i].getAttribute('href');
+		let targetElement = document.getElementById(href.replace('#', ''));
+		const rect = targetElement.getBoundingClientRect().top;
+		const offset = window.pageYOffset;
+		const gap = 0;
+		const target = rect + offset - gap;
+		window.scrollTo({
+			top: target,
+				behavior: 'smooth',
+		});
+	});
+}
