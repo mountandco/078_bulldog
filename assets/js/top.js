34f42df4a7de0992ef0ee0233ad941b7fbdc26ad
坂井 makoto <>
2022-08-30 15:15:43 +0900

    toFix => news slide

--- assets/js/top.js
+++ assets/js/top.js
@@ -27,7 +27,7 @@ var slider1 = new Swiper ('.hero__swiper .swiper', {
 });
 
 // TOPお知らせ
-var slider2 = new Swiper ('.important-information__swiper .swiper', {
+var slider2 = new Swiper ('.js-news-swiper', {
   direction: 'vertical',
   effect: 'slide',
   slidesPerView: 1,
@@ -35,6 +35,27 @@ var slider2 = new Swiper ('.important-information__swiper .swiper', {
   autoplay: {
     delay: 2000,
   },
+});
+
+$(window).on('load', function() {
+  $('.stopbtn').on('click', function() {
+    console.log('button--open => click');
+      let _class = $(this).attr('class');
+      if (_class == 'button--open stopbtn stop') {
+        console.log('.important-information__swiper => Stop');
+        $(this).addClass('start').removeClass('stop');
+        slider2.autoplay.stop();
+        //slider2.destroy(false,true);
+        $('.js-important-information').addClass('acmenu-open');
+      } else {
+        $(this).addClass('stop').removeClass('stop-opacity');
+        $(this).addClass('stop').removeClass('start');
+        $('.js-important-information').removeClass('acmenu-open');
+        //slider2.init();
+        slider2.autoplay.start(); 
+        console.log('.important-information__swiper => Start');
+      }
+  });
 });
 
 // カード型
